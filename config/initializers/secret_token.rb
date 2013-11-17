35fba85b626f0f81331c37805e986e86dfe96c9c
Johannes Thorn <dante@g4t3.de>
2013-11-17 23:24:41 +0100

    Add initial generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MediaBarter::Application.config.secret_key_base = '023d6772b276c2480da91f9ded41e37d9b8adeaa40e35d68cec3939ae70542fe31dfb761ffb0a5cd9f08ec6af08b9ad426ccb8ff0e224ef9ff664532ec1ca47a'
